5c751eed66836a52ac4bf7f0131cb570f6f3eab3
MD MAMUN <mamun757@gmail.com>
2022-01-07 16:10:12 -0500

    save

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         glue ={"step_definitions"},
         plugin = {"pretty","summary","json:target/cucumber.json"},
         snippets = CucumberOptions.SnippetType.CAMELCASE,
-        tags = "@internetAvailability_test"
+        tags = ""
 )
 
 public class TestRunner {
